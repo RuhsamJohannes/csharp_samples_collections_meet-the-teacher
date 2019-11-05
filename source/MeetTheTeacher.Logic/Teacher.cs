82844fad19ab4355c9a6b2b001b85111952d0787
Johannes Ruhsam <johannes.ruhsam@gmx.at>
2019-11-05 11:05:57 +0100

    basic Teacher and TeacherWithDetail implemented

--- source/MeetTheTeacher.Logic/Teacher.cs
+++ source/MeetTheTeacher.Logic/Teacher.cs
@@ -11,5 +11,26 @@ namespace MeetTheTeacher.Logic
     /// </summary>
     public class Teacher
     {
+        private string _name;
+        private string _weekday;
+        private string _time;
+        private string _room;
+        private string _comment;
+
+        public Teacher(string name, string weekday, string time, string room, string comment)
+        {
+            _name = name;
+            _weekday = weekday;
+            _room = room;
+            _time = time;
+            _comment = comment;
+        }
+
+        public string Name { get => _name; }
+
+        public virtual string GetHtmlForName()
+        {
+            return Name;
+        }
     }
 }
--- source/MeetTheTeacher.Logic/TeacherWithDetail.cs
+++ source/MeetTheTeacher.Logic/TeacherWithDetail.cs
@@ -8,7 +8,17 @@ namespace MeetTheTeacher.Logic
     /// <summary>
     /// Klasse, die einen Detaileintrag mit Link auf dem Namen realisiert.
     /// </summary>
-    public class TeacherWithDetail 
+    public class TeacherWithDetail : Teacher
     {
+        private int _id;
+
+        public TeacherWithDetail(string name, string weekday, string time, string room, string comment, int id) : base(name, weekday, time, room, comment)
+        {
+            _id = id;
+        }
+        public override string GetHtmlForName()
+        {
+            return $"<a href=\"?id=999\">{Name}</a>";
+        }
     }
 }
